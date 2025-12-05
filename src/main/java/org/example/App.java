c5e68a4b8c85297c879bbd648a4443028f915cae
leeseonju12 <seonju220@gmail.com>
2025-12-05 14:26:31 +0900

    삭제, 수정, detail 출력 로직 추가

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,12 @@
 package org.example;
-import java.util.ArrayList;
-import java.util.List;
+
+import org.example.motivation.controller.MotivationController;
+import org.example.system.controller.SystemController;
+
 import java.util.Scanner;
 
 public class App {
+
     private Scanner sc;
 
     public App(Scanner sc) {
@@ -12,17 +15,16 @@ public class App {
 
     public void run() {
         System.out.println("== motivation 실행 ==");
-        int lastId = 0;
 
-        //인용문 ArrayList 설정
-        List<Motivation> motivations = new ArrayList<>();
+        SystemController systemController = new SystemController();
+        MotivationController motivationController = new MotivationController(sc);
 
         while (true) {
             System.out.print("명령어 ) ");
             String cmd = sc.nextLine().trim();
 
             if (cmd.equals("exit")) {
-                System.out.println("== motivation 종료 ==");
+                systemController.exit();
                 break;
             } else if (cmd.length() == 0) {
                 System.out.println("명령어가 입력되지 않았음");
@@ -30,60 +32,22 @@ public class App {
             }
 
             if (cmd.equals("add")) {
-//                String[]  = new String[lastId];
-//                String[] source = new String[lastId];
-//                for (int i = 0; i < lastId; i++) {
-//                    System.out.print("motivation : ");
-//                    motivation[i] = sc.nextLine();
-//                    System.out.print("source : ");
-//                    source[i] = sc.nextLine();
-//                }
-                int id = lastId + 1;
-                System.out.print("body : ");
-                String body = sc.nextLine();
-                System.out.print("source : ");
-                String source = sc.nextLine();
-
-                Motivation motivation = new Motivation();
-                motivation.id = id;
-                motivation.body = body;
-                motivation.source = source;
-
-                motivations.add(motivation);
-
-                System.out.printf("%d번 motivation이 등록되었습니다\n", id);
-                lastId++;
+                motivationController.add();
             } else if (cmd.equals("list")) {
-                System.out.println("=".repeat(40));
-                System.out.printf("   번호   /    source    /    motivation   \n");
-//                for (int i = 0; i < lastId; i++) {
-//                    System.out.println(lastId + motivation[i] + source[i] + "\n");
-//                }
-                System.out.println("=".repeat(40));
+                motivationController.list();
+            } else if (cmd.startsWith("del")) {
+                motivationController.delete(cmd);
+            } else if (cmd.startsWith("edit")) {
+                motivationController.edit(cmd);
+            } else if (cmd.startsWith("detail")) {
+                motivationController.detail(cmd);
+            }
 
-                if (motivations.size() == 0) {
-                    System.out.println("등록된거 없음 xxxxx");
-                } else {
-                    System.out.println("1개 이상 있음 oooo");
-
-                }
+            else {
+                System.out.println("사용할 수 없는 명령어야");
             }
         }
+
+
     }
 }
-
-    class Motivation {
-        int id;
-
-        @Override
-        public String toString() {
-            return "Motivation{" +
-                    "id=" + id +
-                    ", body='" + body + '\'' +
-                    ", source='" + source + '\'' +
-                    '}';
-        }
-
-        String body;
-        String source;
-        }
---
+++ src/main/java/org/example/motivation/controller/MotivationController.java
@@ -0,0 +1,183 @@
+package org.example.motivation.controller;
+
+import org.example.motivation.entity.Motivation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MotivationController {
+
+    int lastId = 0; // 몇 번까지 썼더라?
+    List<Motivation> motivations = new ArrayList<>(); // motivation 저장소
+
+    private Scanner sc;
+
+    public MotivationController(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public void add() {
+        int id = lastId + 1;
+        System.out.print("motivation : ");
+        String body = sc.nextLine();
+        System.out.print("source : ");
+        String source = sc.nextLine();
+
+        Motivation motivation = new Motivation(id, body, source);
+
+        motivations.add(motivation);
+
+        System.out.printf("%d번 motivation이 등록되었습니다.\n", id);
+        lastId++;
+    }
+
+    public void list() {
+        System.out.println("=".repeat(40));
+        System.out.printf("   번호   /    source    /    motivation   \n");
+
+        if (motivations.size() == 0) {
+            System.out.println("등록된거 없음 xxxxx");
+            return;
+        }
+        for (int i = motivations.size() - 1; i >= 0; i--) {
+            Motivation motivation = motivations.get(i);
+
+            if (motivation.getSource().length() > 7) {
+                System.out.printf("   %d     /    %s       /     %s   \n", motivation.getId(), motivation.getSource().substring(0, 7) + "...", motivation.getBody());
+                continue;
+            }
+            System.out.printf("   %d     /    %s       /     %s   \n", motivation.getId(), motivation.getSource(), motivation.getBody());
+        }
+        System.out.println("=".repeat(40));
+    }
+
+
+    //    public void delete(String cmd) {
+//        String[] cmdBits = cmd.split(" ");
+//        int id = Integer.parseInt(cmdBits[1]);
+//
+//        if (cmdBits.length == 1) {
+//            System.out.println("명령어 확인하고 다시 써");
+//            return;
+//        }
+//
+//        Motivation foundMotivation = null;
+//
+//        for (Motivation motivation : motivations) {
+//            if (motivation.getId() == id) {
+//                foundMotivation = motivation;
+//                break;
+//            }
+//        }
+//
+//        if (foundMotivation == null) {
+//            System.out.println("해당 moti는 ArrayList에 없던데?");
+//            return;
+//        }
+//
+//        motivations.remove(foundMotivation);
+//        System.out.println(id + "번 moti 삭제됨");
+//    }
+
+    public void delete(String cmd) {
+        String[] cmdBits = cmd.split(" ");
+        int id = Integer.parseInt(cmdBits[1]);
+
+        if (cmdBits.length == 1) {
+            System.out.println("명령어 확인하고 다시 써");
+            return;
+        }
+
+        int foundIndex = -1;
+
+        for (int i = 0; i < motivations.size(); i++) {
+            Motivation motivation = motivations.get(i);
+            if (motivation.getId() == id) {
+                foundIndex = i;
+                break;
+            }
+        }
+
+        if (foundIndex == -1) {
+            System.out.println(id + "번 motivation은 존재하지 않습니다.");
+            return;
+        }
+
+        motivations.remove(foundIndex);
+        System.out.println(id + "번 motivation이 삭제되었습니다.");
+    }
+
+
+    public void edit(String cmd) {
+        // delete 로직과 동일한 부분
+        String[] cmdBits = cmd.split(" ");
+
+        if (cmdBits.length == 1) {
+            System.out.println("명령어 확인하고 다시 써");
+            return;
+        }
+
+        int id = Integer.parseInt(cmdBits[1]);
+        int foundIndex = -1;
+
+        for (int i = 0; i < motivations.size(); i++) {
+            Motivation motivation = motivations.get(i);
+            if (motivation.getId() == id) {
+                foundIndex = i;
+                break;
+            }
+        }
+
+        if (foundIndex == -1) {
+            System.out.println(id + "번 motivation은 존재하지 않습니다.");
+            return;
+        }
+
+
+        // 새로 수정된 edit 로직
+        Motivation motivation = motivations.get(foundIndex);
+
+        System.out.print("new motivation : ");
+        String body = sc.nextLine();
+
+        System.out.print("new source : ");
+        String source = sc.nextLine();
+
+        motivation.setBody(body);
+        motivation.setSource(source);
+
+        System.out.println(id + "번 motivation이 수정되었습니다.");
+    }
+
+    public void detail(String cmd) {
+        String[] cmdBits = cmd.split(" ");
+        int id = Integer.parseInt(cmdBits[1]);
+
+        if (cmdBits.length == 1) {
+            System.out.println("명령어 확인하고 다시 써");
+            return;
+        }
+
+        int foundIndex = -1;
+
+        for (int i = 0; i < motivations.size(); i++) {
+            Motivation motivation = motivations.get(i);
+            if (motivation.getId() == id) {
+                foundIndex = i;
+                break;
+            }
+        }
+
+        if (foundIndex == -1) {
+            System.out.println(id + "번 motivation은 존재하지 않습니다.");
+            return;
+        }
+
+        Motivation motivation = motivations.get(foundIndex);
+        System.out.println("--- detail ---");
+        System.out.printf("번호: %d\nsource: %s\nmotivation: %s\n", motivation.getId(), motivation.getSource(), motivation.getBody());
+        System.out.println(id + "번 motivation의 detail이 출력되었습니다.");
+
+    }
+}
